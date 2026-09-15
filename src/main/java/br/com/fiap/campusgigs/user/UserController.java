9e63a22e66ebd3eb002e55da241ce3fe3546279c
yJoaoVictor10 <jvna07@gmail.com>
2026-09-14 22:17:54 -0300

    project finished

--- src/main/java/br/com/fiap/campusgigs/user/UserController.java
+++ src/main/java/br/com/fiap/campusgigs/user/UserController.java
@@ -2,8 +2,11 @@ package br.com.fiap.campusgigs.user;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
@@ -33,6 +36,54 @@ public class UserController {
 
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         user.setRole("USER");
+
+        return userRepository.save(user);
+    }
+
+    @PutMapping("/{id}")
+    public Users update(
+            @PathVariable Long id,
+            @RequestBody Users userData,
+            @AuthenticationPrincipal Jwt jwt
+    ) {
+        String username = jwt.getSubject();
+
+        Users user = userRepository.findById(id)
+                .orElseThrow(() ->
+                        new ResponseStatusException(
+                                HttpStatus.NOT_FOUND,
+                                "Usuário não encontrado"
+                        )
+                );
+
+        if (!user.getUsername().equals(username)) {
+            throw new ResponseStatusException(
+                    HttpStatus.FORBIDDEN,
+                    "Você só pode atualizar seus próprios dados"
+            );
+        }
+
+        UserResponse response;
+
+        try {
+            response = userService.getCep(userData.getCep());
+        } catch (RestClientException e) {
+            throw new ResponseStatusException(
+                    HttpStatus.SERVICE_UNAVAILABLE,
+                    "Serviço de CEP indisponível no momento"
+            );
+        }
+
+        if (Boolean.TRUE.equals(response.error())) {
+            throw new ResponseStatusException(
+                    HttpStatus.BAD_REQUEST,
+                    "CEP não encontrado"
+            );
+        }
+
+        user.setCep(userData.getCep());
+        user.setLocation(response.location());
+        user.setUf(response.uf());
 
         return userRepository.save(user);
     }
